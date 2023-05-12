f7f3b2905770bd9df520f3e4eb0dcf29cc03773f
First Medtrade Africa <info@firstmedtradeafrica.com>
2023-05-12 12:05:01 +0100

    asfa

--- vite.config.js
+++ vite.config.js
@@ -35,17 +35,17 @@ export default defineConfig({
           }
         ],
       },
-      workbox: {
-        globDirectory: 'dist',
-        globPatterns: ['**/*'],
-        swDest: 'dist/sw.js',
-        additionalManifestEntries: [
-          { url: 'index.html', revision: null } // Precache index.html without revisioning
-        ]
-      },
-      pwa: {
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // Set the limit to 5MB (or adjust as needed)
-      }
+      // workbox: {
+      //   globDirectory: 'dist',
+      //   globPatterns: ['**/*'],
+      //   swDest: 'dist/sw.js',
+      //   additionalManifestEntries: [
+      //     { url: 'index.html', revision: null } // Precache index.html without revisioning
+      //   ]
+      // },
+      // pwa: {
+      //   maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // Set the limit to 5MB (or adjust as needed)
+      // }
     })
   ],
 })
